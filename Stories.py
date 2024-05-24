afa5351219a06d17ed378371d331844df21a87d4
Bruno Teixeira <166347569+TXzzera@users.noreply.github.com>
2024-05-24 01:39:50 -0300

    Add files via upload
    Prove W01 Clever Stories

---
+++ Stories.py
@@ -0,0 +1,19 @@
+print ('Please, enter the following information:')
+print()
+
+adjective = input ('Adjective:')
+animal  = input ('Animal:')
+verb1 = input ('Verb:')
+exclamation = input ('Exclamation:')
+verb2 = input ('Verb:')
+verb3 = input ('Verb:')
+
+#This time I used a\n to make a blank line before this:
+print('\nYour story is:')
+print('The other day, I was really in a big trouble. It all started when I saw a very ')
+print(f'{adjective} {animal} {verb1} down the hallway.'f'{exclamation.capitalize()}!''I yelled hopelessly. But all')
+print(f'I could think to do was to {verb2} over and over. Miraculously,')
+print(f'that caused it to stop, but not before it tried to {verb3}')
+print('right in front of my family.')
+
+#I just had add some new adverbs to strenghten the intensity of the phrases.
