fa47e4272edf1856143ac0db70e6fe69a29b45a4
Bruno Teixeira <166347569+TXzzera@users.noreply.github.com>
2024-06-11 11:04:41 -0300

    Add files via upload

---
+++ Id-badge.py
@@ -0,0 +1,20 @@
+print ('Please, enter the following information:')
+print()
+
+first = input ('First Name:')
+last = input ('Last Name:')
+email = input ('Email adress:')
+phone = input ('Phone number:')
+job_title = input ('Job title:')
+id_number = input ('ID Number:')
+
+#This time I used a\n to make a blank line before this:
+print('\nThe ID Card is:')
+print('---------------------------------------')
+print(f"{last.upper()}, {first.capitalize()}")
+print(job_title.title())
+print(f'ID: {id_number}')
+print()
+print(email.lower())
+print (phone)
+print ('------------------------------------------')
